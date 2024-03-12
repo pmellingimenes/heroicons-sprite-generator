682fdd6c7610828231818cad838b6d4415cb543b
Paulo Mellin Gimenes <pmellingimenes@gmail.com>
2024-03-12 19:22:37 -0300

    Add icon type to icon id

--- svg_sprite_generator.sh
+++ svg_sprite_generator.sh
@@ -10,6 +10,7 @@ process_svg_files() {
     local SVG_DIR=$1
     local OUTPUT_FILE=$2
     local ICON_SIZE=$3
+    local ICON_TYPE=$4
 
     echo '<svg style="display: none;">' > "$OUTPUT_FILE"
 
@@ -17,7 +18,7 @@ process_svg_files() {
         FILENAME=$(basename "$file" .svg)
         SVG_CONTENT=$(sed '/<?xml/d;/<!DOCTYPE/d;/<svg/d;/<\/svg/d' "$file")
 
-        echo "<symbol id=\"heroicon-$FILENAME\" viewBox=\"0 0 $ICON_SIZE $ICON_SIZE\">" >> "$OUTPUT_FILE"
+        echo "<symbol id=\"heroicon-$ICON_TYPE-$FILENAME\" viewBox=\"0 0 $ICON_SIZE $ICON_SIZE\">" >> "$OUTPUT_FILE"
         echo $SVG_CONTENT >> "$OUTPUT_FILE"
         echo "</symbol>" >> "$OUTPUT_FILE"
     done
@@ -28,7 +29,7 @@ process_svg_files() {
     echo "SVG icons have been processed and saved to $OUTPUT_FILE"
 }
 
-process_svg_files "heroicons/src/24/solid/" "${MOUNTED_DIR}/build/icons-solid.svg" 24
-process_svg_files "heroicons/src/24/outline/" "${MOUNTED_DIR}/build/icons-outline.svg" 24
-process_svg_files "heroicons/src/20/solid/" "${MOUNTED_DIR}/build/icons-mini.svg" 20
-process_svg_files "heroicons/src/16/solid/" "${MOUNTED_DIR}/build/icons-micro.svg" 16
+process_svg_files "heroicons/src/24/solid/" "${MOUNTED_DIR}/build/icons-solid.svg" 24 "solid"
+process_svg_files "heroicons/src/24/outline/" "${MOUNTED_DIR}/build/icons-outline.svg" 24 "outline"
+process_svg_files "heroicons/src/20/solid/" "${MOUNTED_DIR}/build/icons-mini.svg" 20 "mini"
+process_svg_files "heroicons/src/16/solid/" "${MOUNTED_DIR}/build/icons-micro.svg" 16 "micro"
